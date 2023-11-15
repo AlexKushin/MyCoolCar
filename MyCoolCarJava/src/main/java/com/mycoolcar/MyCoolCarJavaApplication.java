55aa47c8cf2bcf3551c46b346ba3b8b22ed1f20e
kyshin <kyshin1993@gmail.com>
2023-11-15 19:03:26 +0100

    Init commit

---
+++ MyCoolCarJava/src/main/java/com/mycoolcar/MyCoolCarJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mycoolcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoolCarJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoolCarJavaApplication.class, args);
+	}
+
+}
---
+++ MyCoolCarJava/src/test/java/com/mycoolcar/MyCoolCarJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycoolcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolCarJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
