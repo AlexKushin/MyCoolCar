33357db5ec50c3afeddd8687ea6708a3bf1a9094
kyshin <kyshin1993@gmail.com>
2024-02-22 22:53:20 +0100

    backend: CarController - method for creating new car entities has been refactored: image files store in google cloud storage, urls store in CarEntity (database)

--- MyCoolCarJava/src/main/java/com/mycoolcar/controllers/CarController.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/controllers/CarController.java
@@ -4,13 +4,16 @@ import com.mycoolcar.dtos.CarCreationDto;
 import com.mycoolcar.entities.CarEntity;
 import com.mycoolcar.entities.User;
 import com.mycoolcar.services.CarService;
+import com.mycoolcar.services.GoogleFileServiceImpl;
 import com.mycoolcar.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
@@ -25,21 +28,44 @@ public class CarController {
 
     private final UserService userService;
 
+    private final GoogleFileServiceImpl fileService;
+
     @Autowired
-    public CarController(CarService carService, UserService userService) {
+    public CarController(CarService carService, UserService userService, GoogleFileServiceImpl fileService) {
         this.carService = carService;
         this.userService = userService;
+        this.fileService = fileService;
     }
 
     @GetMapping("cars")
     public List<CarCreationDto> getAllCars() {
         return carService.getAllCars();
     }
+
     @PostMapping("cars")
-    public ResponseEntity<CarEntity> postCar(Principal principal, @RequestBody CarCreationDto carCreationDto) {
+    public ResponseEntity<CarEntity> postCar(Principal principal,
+                                             @RequestPart("files[]") MultipartFile[] images,
+                                             @RequestPart("mainImage") MultipartFile mainImage,
+                                             @RequestParam("brand") String carBrand,
+                                             @RequestParam("model") String carModel,
+                                             @RequestParam("productYear") Integer carProductYear,
+                                             @RequestParam("description") String carDescription) throws IOException {
+
         Optional<User> userOptional = userService.getByUsername(principal.getName());
-        CarEntity newCar = new CarEntity(carCreationDto.brand(), carCreationDto.model(),
-                carCreationDto.productYear(), carCreationDto.description(), carCreationDto.image());
+
+        CarEntity newCar = new CarEntity(carBrand, carModel,
+                carProductYear, carDescription);
+        if(!mainImage.isEmpty()){
+            String mainImageUrl = fileService.uploadFile(mainImage);
+            newCar.setMainImageUrl(mainImageUrl);
+        }
+        if(images.length > 0){
+            String[] imagesUrls = new String[8];
+            for (int i = 0; i < images.length; i++) {
+                imagesUrls[i] = fileService.uploadFile(images[i]);
+            }
+            newCar.setImagesUrl(imagesUrls);
+        }
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             user.addCar(newCar);
