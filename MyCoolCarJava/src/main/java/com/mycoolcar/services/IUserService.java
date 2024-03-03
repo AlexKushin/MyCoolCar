dda1f96b41ce253e008f82d2d20e972607f075e1
kyshin <kyshin1993@gmail.com>
2024-03-03 23:36:37 +0100

    backend: IUserService - interface for work with user entity, contains necessary methods to work with registration and verification token UserService has been refactored. Methods which execute necessary logic tied with registration and registration confirmation via verification token have been implemented

---
+++ MyCoolCarJava/src/main/java/com/mycoolcar/services/IUserService.java
@@ -0,0 +1,21 @@
+package com.mycoolcar.services;
+
+import com.mycoolcar.dtos.UserCreationDto;
+import com.mycoolcar.entities.User;
+import com.mycoolcar.entities.VerificationToken;
+import com.mycoolcar.exceptions.UserAlreadyExistException;
+
+import java.util.Optional;
+
+public interface IUserService {
+    Optional<User> registerNewUserAccount(UserCreationDto userDto)
+            throws UserAlreadyExistException;
+
+    User getUser(String verificationToken);
+
+    void saveRegisteredUser(User user);
+
+    void createVerificationTokenForUser(User user, String token);
+
+    VerificationToken getVerificationToken(String verificationToken);
+}
--- MyCoolCarJava/src/main/java/com/mycoolcar/services/UserService.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/services/UserService.java
@@ -1,39 +1,51 @@
 package com.mycoolcar.services;
 
 import com.mycoolcar.dtos.UserCreationDto;
-import com.mycoolcar.entities.User;
 import com.mycoolcar.entities.Role;
+import com.mycoolcar.entities.User;
+import com.mycoolcar.entities.VerificationToken;
+import com.mycoolcar.exceptions.ResourceNotFoundException;
 import com.mycoolcar.exceptions.UserAlreadyExistException;
-import com.mycoolcar.repositories.UserRepository;
 import com.mycoolcar.repositories.RoleRepository;
+import com.mycoolcar.repositories.UserRepository;
+import com.mycoolcar.repositories.VerificationTokenRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+
 @Slf4j
 @Service
-public class UserService  implements UserDetailsService {
+public class UserService implements UserDetailsService, IUserService {
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String ROLE_USER = "USER";
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
+    private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+
+
     @Autowired
     public UserService(UserRepository userRepository,
-                       RoleRepository roleRepository) {
+                       RoleRepository roleRepository,
+                       PasswordEncoder passwordEncoder,
+                       VerificationTokenRepository verificationTokenRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.verificationTokenRepository = verificationTokenRepository;
     }
 
     public void initRolesAndUsers() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         Role adminRole = new Role();
         adminRole.setRoleName(ROLE_ADMIN);
         adminRole.setRoleDescription("admin role");
@@ -48,7 +60,7 @@ public class UserService  implements UserDetailsService {
         admin.setFirstName("admin");
         admin.setLastName("admin");
         admin.setEmail("admin@gmail.com");
-        admin.setPassword(encoder.encode("password"));
+        admin.setPassword(passwordEncoder.encode("password"));
         Set<Role> personRoles = new HashSet<>();
         personRoles.add(adminRole);
         admin.setRoles(personRoles);
@@ -58,7 +70,7 @@ public class UserService  implements UserDetailsService {
         user.setFirstName("user");
         user.setLastName("user");
         user.setEmail("user@gmail.com");
-        user.setPassword(encoder.encode("password"));
+        user.setPassword(passwordEncoder.encode("password"));
         Set<Role> userRoles = new HashSet<>();
         userRoles.add(userRole);
         user.setRoles(userRoles);
@@ -68,20 +80,21 @@ public class UserService  implements UserDetailsService {
     public Optional<User> getByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
     public Optional<User> getByUsername(String username) {
         return userRepository.findUserByFirstName(username);
     }
 
-    public Optional<User> registerNewUser(UserCreationDto userCreationDto) throws UserAlreadyExistException{
-        userRepository.findByEmail(userCreationDto.email()).ifPresent(person -> {
-            throw new UserAlreadyExistException("User with ID: " + person.getEmail() + " is already exists");
+    @Override
+    public Optional<User> registerNewUserAccount(UserCreationDto userCreationDto) throws UserAlreadyExistException {
+        userRepository.findByEmail(userCreationDto.email()).ifPresent(user -> {
+            throw new UserAlreadyExistException("User with ID: " + user.getEmail() + " is already exists");
         });
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         User newUser = new User();
         newUser.setFirstName(userCreationDto.firstName());
         newUser.setLastName(userCreationDto.lastName());
         newUser.setEmail(userCreationDto.email());
-        newUser.setPassword(encoder.encode(userCreationDto.password()));
+        newUser.setPassword(passwordEncoder.encode(userCreationDto.password()));
         Set<Role> personRoles = newUser.getRoles();
         Role role = roleRepository.findByRoleName(ROLE_USER);
         personRoles.add(role);
@@ -91,13 +104,37 @@ public class UserService  implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("user was not found")/*createUserIfNotExists()*/);
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user was not found"));
     }
 
     public UserDetails loadUserById(long id) {
-        return userRepository.findById(id).orElseThrow(/*() -> new ResourceNotFoundException("User", "id", id)*/);
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User with id =" + id + " not found"));
     }
-    public User save(User user){
+
+    public User save(User user) {
         return userRepository.save(user);
+    }
+
+
+    @Override
+    public User getUser(String verificationToken) {
+        return verificationTokenRepository.findByToken(verificationToken).getUser();
+    }
+
+    @Override
+    public void saveRegisteredUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void createVerificationTokenForUser(User user, String token) {
+        final VerificationToken myToken = new VerificationToken(token, user);
+        verificationTokenRepository.save(myToken);
+    }
+
+    @Override
+    public VerificationToken getVerificationToken(String verificationToken) {
+        return verificationTokenRepository.findByToken(verificationToken);
     }
 }
