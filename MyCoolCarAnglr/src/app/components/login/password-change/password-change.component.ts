405f94bcb249750c5e892965e42cab610699ad46
kyshin <kyshin1993@gmail.com>
2024-03-05 14:23:56 +0100

    frontend: password-reset.component has been created - component for initiating password resetting password-change.component - component for imputing new user's password after proceeding by link from email

---
+++ MyCoolCarAnglr/src/app/components/login/password-change/password-change.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-password-change',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './password-change.component.html',
+  styleUrls: ['./password-change.component.css']
+})
+export class PasswordChangeComponent {
+
+}
