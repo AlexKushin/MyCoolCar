212aa4ad99156db508e0fddb13ac382f5a212180
kyshin <kyshin1993@gmail.com>
2024-03-25 15:01:13 +0100

    Backend: MvcConfig - bean fo CsvReader has been added CarModelListService - method importToDB()has been refactored and split for better readability, exception handling has been moved to RestResponseEntityExceptionHandler

--- MyCoolCarJava/src/main/java/com/mycoolcar/configs/MvcConfig.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/configs/MvcConfig.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -55,9 +57,10 @@ public class MvcConfig implements WebMvcConfigurer {
         return new PasswordMatchesValidator();
     }
 
-   /* @Bean
-    public CSVReader csvReader (){
-        return new CSVReader(new FileReader(csvFilePath, StandardCharsets.UTF_8)));
-    }*/
+    @Bean
+    public CSVReader csvReader() throws IOException {
+        String filePath = "MyCoolCarJava/src/main/resources/car_models_list.csv";
+        return new CSVReader(new FileReader(filePath, StandardCharsets.UTF_8));
+    }
 
 }
--- MyCoolCarJava/src/main/java/com/mycoolcar/services/CarModelListService.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/services/CarModelListService.java
@@ -11,56 +11,53 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Set;
 
 @Slf4j
 @Service
 public class CarModelListService {
-
     private final CarBrandRepository carBrandRepository;
     private final CarModelRepository carModelRepository;
+    private final CSVReader reader;
 
     @Autowired
     public CarModelListService(CarBrandRepository carBrandRepository,
-                               CarModelRepository carModelRepository) {
+                               CarModelRepository carModelRepository,
+                               CSVReader reader) {
         this.carBrandRepository = carBrandRepository;
         this.carModelRepository = carModelRepository;
+        this.reader = reader;
     }
 
     @PostConstruct
-    public void importCarModelsToDB() {
-        try (CSVReader reader = new CSVReader(new FileReader(
-                "MyCoolCarJava/src/main/resources/car_models_list.csv", StandardCharsets.UTF_8))) {
-            String[] carBrandModels;
-            while ((carBrandModels = reader.readNext()) != null) {
-                String carBrandName = carBrandModels[0];
-                CarBrand carBrand = new CarBrand(carBrandName);
-                carBrandRepository.save(carBrand);
-                Set<CarModel> carModels = carBrand.getCarModels();
-                for (int i = 1; i < carBrandModels.length; i++) {
-                    String carModelName = carBrandModels[i];
-                    CarModel carModel = new CarModel(carModelName);
-                    carModel.setCarBrand(carBrand);
-                    carModelRepository.save(carModel);
-                    carModels.add(carModel);
-                }
-                carBrand.setCarModels(carModels);
-                carBrandRepository.save(carBrand);
-            }
-            log.info("Data was successfully imported from CSV file to DataBase");
-        } catch (FileNotFoundException e) {
-            log.error("There is no file to read {}", e.getMessage());
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            log.error("Can't read file by path");
-        } catch (CsvValidationException e) {
-            throw new RuntimeException(e);
+    public void importCarModelsToDB() throws IOException, CsvValidationException {
+        String[] carBrandModels;
+        while ((carBrandModels = reader.readNext()) != null) {
+            importCarBrandWithModels(carBrandModels);
         }
+        log.info("Data was successfully imported from CSV file to DataBase");
+    }
 
+    private void importCarBrandWithModels(String[] carBrandModels) {
+        if (carBrandModels.length < 2) {
+            log.warn("Skipping invalid entry in CSV: " + Arrays.toString(carBrandModels));
+            return;
+        }
+        String carBrandName = carBrandModels[0];
+        CarBrand carBrand = new CarBrand(carBrandName);
+        carBrandRepository.save(carBrand);
+        Set<CarModel> carModels = carBrand.getCarModels();
+        for (int i = 1; i < carBrandModels.length; i++) {
+            String carModelName = carBrandModels[i];
+            CarModel carModel = new CarModel(carModelName);
+            carModel.setCarBrand(carBrand);
+            carModelRepository.save(carModel);
+            carModels.add(carModel);
+        }
+        carBrand.setCarModels(carModels);
+        carBrandRepository.save(carBrand);
     }
 }
 
