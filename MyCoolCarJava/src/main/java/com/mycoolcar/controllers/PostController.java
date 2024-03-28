8017514837b39f05a90b004709a925931d98f051
kyshin <kyshin1993@gmail.com>
2024-03-28 21:58:23 +0100

    Backend: CarClubPostRepository and CarLogBookPostRepository have been renamed PostService - methods for deleting posts have been added PostController - endpoint for deleting CarLogBookPost and CarClubPost have been added, they can be used by admin or regular user

--- MyCoolCarJava/src/main/java/com/mycoolcar/controllers/PostController.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/controllers/PostController.java
@@ -3,12 +3,14 @@ package com.mycoolcar.controllers;
 import com.mycoolcar.dtos.CarLogPostDto;
 import com.mycoolcar.dtos.ClubPostDto;
 import com.mycoolcar.entities.*;
+import com.mycoolcar.exceptions.ApiResponse;
 import com.mycoolcar.repositories.CarClubRepository;
 import com.mycoolcar.repositories.CarLogbookRepository;
 import com.mycoolcar.services.PostService;
 import com.mycoolcar.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @Slf4j
@@ -30,13 +33,16 @@ public class PostController {
 
     private final UserService userService;
 
+    private final MessageSource messageSource;
+
     @Autowired
     public PostController(PostService postService, CarLogbookRepository carLogbookRepository,
-                          CarClubRepository carClubRepository, UserService userService) {
+                          CarClubRepository carClubRepository, UserService userService, MessageSource messageSource) {
         this.postService = postService;
         this.carLogbookRepository = carLogbookRepository;
         this.carClubRepository = carClubRepository;
         this.userService = userService;
+        this.messageSource = messageSource;
     }
 
     @PostMapping("car-log-posts/new")
@@ -76,6 +82,19 @@ public class PostController {
         Optional<User> user = userService.getByUsername(principal.getName());
         return user.map(value -> new ResponseEntity<>(postService.getNewPosts(value), HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
+    }
+
+    @DeleteMapping({"/car-club-posts/{id}"})
+    public ResponseEntity<ApiResponse> deleteCarClubPost(@PathVariable long id, final Locale locale) {
+        postService.deleteCarClubPost(id);
+        return new ResponseEntity<>(new ApiResponse(
+                messageSource.getMessage("message.deletePost", null, locale)), HttpStatus.OK);
+    }
+    @DeleteMapping({"/car-logbook-posts/{id}"})
+    public ResponseEntity<ApiResponse> deleteCarLogBookPost(@PathVariable long id, final Locale locale) {
+        postService.deleteCarLogPost(id);
+        return new ResponseEntity<>(new ApiResponse(
+                messageSource.getMessage("message.deletePost", null, locale)), HttpStatus.OK);
     }
 
 }
