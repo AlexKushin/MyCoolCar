d9600eb040c9b0dfc12975409a30e515c040a4b7
kyshin <kyshin1993@gmail.com>
2024-04-09 19:58:28 +0200

    Backend: Car - List is used for storage image urls instead of simple array CarCreationDto - changes are tied with changes in Car entity CarService - method addNewCar() has been changed accordingly to changes in Car entity, methods for editing and deleting existing cars have been added CarController - endpoints for editing and deleting existing cars have been added GoogleFileServiceImpl - method for retrieving filename from link has been added, method deleteFile() has been fixed

--- MyCoolCarJava/src/main/java/com/mycoolcar/entities/Car.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/entities/Car.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Null;
 import lombok.*;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @Table(name = "cars")
@@ -33,7 +33,7 @@ public class Car implements Serializable {
 
     private String mainImageUrl;
 
-    private String[] imagesUrl;
+    private List<String> imagesUrl;
 
     private int rate;
 
@@ -60,14 +60,14 @@ public class Car implements Serializable {
         return productYear == car.productYear && rate == car.rate && Objects.equals(id, car.id)
                 && Objects.equals(brand, car.brand) && Objects.equals(model, car.model)
                 && Objects.equals(description, car.description) && Objects.equals(mainImageUrl, car.mainImageUrl)
-                && Arrays.equals(imagesUrl, car.imagesUrl) && Objects.equals(logbook, car.logbook)
+                && Objects.equals(imagesUrl, car.imagesUrl) && Objects.equals(logbook, car.logbook)
                 && Objects.equals(user, car.user);
     }
 
     @Override
     public int hashCode() {
         int result = Objects.hash(id, brand, model, productYear, description, mainImageUrl, rate, logbook, user);
-        result = 31 * result + Arrays.hashCode(imagesUrl);
+        result = 31 * result + Objects.hashCode(imagesUrl);
         return result;
     }
 }
--- MyCoolCarJava/src/main/java/com/mycoolcar/services/CarService.java
+++ MyCoolCarJava/src/main/java/com/mycoolcar/services/CarService.java
@@ -3,6 +3,7 @@ package com.mycoolcar.services;
 import com.mycoolcar.dtos.CarCreationDto;
 import com.mycoolcar.entities.Car;
 import com.mycoolcar.entities.User;
+import com.mycoolcar.exceptions.ResourceNotFoundException;
 import com.mycoolcar.repositories.CarRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Slf4j
@@ -44,9 +47,9 @@ public class CarService {
             newCar.setMainImageUrl(mainImageUrl);
         }
         if (images.length > 0) {
-            String[] imagesUrls = new String[8];
-            for (int i = 0; i < images.length; i++) {
-                imagesUrls[i] = fileService.uploadFile(images[i]);
+            List <String> imagesUrls = new ArrayList<>();
+            for (MultipartFile image : images) {
+                imagesUrls.add(fileService.uploadFile(image));
             }
             newCar.setImagesUrl(imagesUrls);
         }
@@ -55,4 +58,53 @@ public class CarService {
         return newCar;
     }
 
+    public Optional<Car> editCar(Long carId, MultipartFile[] images,
+                                 MultipartFile mainImage, List<String> deletedImages,
+                                 String carBrand, String carModel,
+                                 Integer carProductYear, String carDescription) throws IOException {
+        Optional<Car> car = carRepository.findById(carId);
+        if (car.isEmpty()) {
+            throw new ResourceNotFoundException("Car with id: " + carId + "  is not found");
+        }
+        Car editedCar = car.get();
+        if (!mainImage.isEmpty()) {
+            fileService.deleteFile(editedCar.getMainImageUrl());
+            String mainImageUrl = fileService.uploadFile(mainImage);
+            editedCar.setMainImageUrl(mainImageUrl);
+        }
+        deletedImages.forEach(fileService::deleteFile);
+        if (images.length > 0) {
+            List <String> imagesUrls = editedCar.getImagesUrl();
+            if (imagesUrls == null) {
+                imagesUrls = new ArrayList<>();
+            }
+            for (MultipartFile image : images) {
+                imagesUrls.add(fileService.uploadFile(image));
+            }
+            editedCar.setImagesUrl(imagesUrls);
+        }
+        editedCar.setBrand(carBrand);
+        editedCar.setModel(carModel);
+        editedCar.setProductYear(carProductYear);
+        editedCar.setDescription(carDescription);
+        return Optional.of(carRepository.save(editedCar));
+    }
+
+    public void deleteCar(User user, Long carId) {
+        Optional<Car> car = carRepository.findById(carId);
+        if(car.isEmpty()){
+            throw new ResourceNotFoundException("Car with id: " + carId + "  is not found");
+        }
+        Car deletedCar = car.get();
+        List <String> imagesUrls = deletedCar.getImagesUrl();
+        if(imagesUrls != null && !imagesUrls.isEmpty() ){
+            imagesUrls.forEach(fileService::deleteFile);
+        }
+        String mainImage = deletedCar.getMainImageUrl();
+        if(mainImage != null ){
+            fileService.deleteFile(mainImage);
+        }
+        user.removeCar(deletedCar);
+        carRepository.delete(deletedCar);
+    }
 }
