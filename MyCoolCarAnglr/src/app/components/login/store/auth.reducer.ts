190a68b5ffb9e62d736394fe6508715ee78f2b66
kyshin <kyshin1993@gmail.com>
2024-04-13 14:50:42 +0200

    Frontend: actions, reducer and effects from ngrx Store for Authentication and Registration logic have been added

---
+++ MyCoolCarAnglr/src/app/components/login/store/auth.reducer.ts
@@ -0,0 +1,73 @@
+import {User} from "../../../models/user";
+import * as  AuthActions from "./auth.actions";
+
+export interface State {
+  user: User;
+  authError: string;
+  loading: boolean;
+}
+
+
+const initialState: State = {
+
+// @ts-ignore
+  user: null,
+  // @ts-ignore
+  authError: null,
+  loading: false
+}
+
+
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true
+      };
+    case AuthActions.AUTHENTICATE_FAIL:
+      return {
+        ...state,
+        user: null,
+        authError: action.payload,
+        loading: false
+      };
+
+    case AuthActions.AUTHENTICATED_USER:
+      const user = new User(
+        action.payload.id,
+        action.payload.ban,
+        '',
+        action.payload.firstName,
+        action.payload.lastName,
+        action.payload.email,
+        [],
+        action.payload.enabled
+      );
+      console.log("user = " + user)
+      return {
+        ...state,
+        authError: null,
+        user: user,
+        loading: false
+
+      }
+
+    case AuthActions.REGISTRATION_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true
+      }
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null
+      };
+    case AuthActions.AUTHENTICATE_SUCCESS:
+
+    default:
+      return state;
+  }
+}
