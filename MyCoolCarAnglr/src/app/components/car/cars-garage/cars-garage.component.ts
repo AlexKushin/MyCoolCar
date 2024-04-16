39b5f348a29e2d3b7c6109a278dee91bf621fb55
kyshin <kyshin1993@gmail.com>
2024-04-16 23:49:43 +0200

    frontend: cars-garage.component.ts, cars-list.component.ts - logic for retrieving user car and displaying it has been added

--- MyCoolCarAnglr/src/app/components/car/cars-garage/cars-garage.component.ts
+++ MyCoolCarAnglr/src/app/components/car/cars-garage/cars-garage.component.ts
@@ -1,32 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CarsListComponent} from "./cars-list/cars-list.component";
 import {NewCarComponent} from "./new-car/new-car.component";
-import {ActivatedRoute, Router} from "@angular/router";
-import {UserService} from "../../../services/user.service";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {Store} from "@ngrx/store";
-import * as fromAuth from "../../login/store/auth.reducer";
+import * as fromUserCars from "../store/cars.reducer";
+import {NgIf} from "@angular/common";
+import {map, Subscription} from "rxjs";
+import {Car} from "../../../models/car";
 
 @Component({
   selector: 'app-cars-garage',
   standalone: true,
   imports: [
     CarsListComponent,
-    NewCarComponent
+    NewCarComponent,
+    NgIf,
+    RouterLink
   ],
   templateUrl: './cars-garage.component.html',
   styleUrl: './cars-garage.component.css'
 })
-export class CarsGarageComponent {
+export class CarsGarageComponent implements OnInit, OnDestroy {
+  isMyCars = true;
+
+  public userCars: Car[];
+  public userSubscribedCars: Car[];
+  subscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-
-    private store: Store<{ auth: fromAuth.State }>
+    private store: Store<{ userCarsState: fromUserCars.State }>
   ) {
   }
+
+  ngOnInit(): void {
+    this.subscription = this.store.select('userCarsState')
+      .pipe(map(userCarsState => userCarsState.userCars))
+      .subscribe((userCars: Car[]) => this.userCars = userCars)
+  }
+
+  ngOnDestroy(): void {
+
+    this.subscription.unsubscribe();
+  }
+
   addCar() {
     this.router.navigate(['cars/new'])
   }
 
+  showMyCars() {
+    this.isMyCars = true
+  }
+
+  showSubscribedCars() {
+    this.isMyCars = false
+  }
 }
